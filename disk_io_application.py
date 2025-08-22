b07ab0a80e798203bc4e132ee93f91bec29aef06
NikulinOV <oleg309@yandex.ru>
2025-08-22 14:18:41 +0300

    Новая конфигурация

---
+++ disk_io_application.py
@@ -0,0 +1,13 @@
+import os
+
+
+# Function to generate dummy disk I/O operations
+def generate_disk_io_operations():
+    filename = "dummy_file.txt"
+    with open(filename, "w") as f:
+        for i in range(10000000000000):  # Adjust number of iterations for more I/O
+            f.write(f"This is line {i}\n")
+    os.remove(filename)
+
+
+generate_disk_io_operations()
