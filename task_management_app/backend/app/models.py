d385f640c75468e282770c725d0261dcaf63ea05
NikulinOV <oleg309@yandex.ru>
2025-08-25 17:23:11 +0300

    Новая конфигурация

---
+++ task_management_app/backend/app/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    tasks = db.relationship('Task', backref='owner', lazy=True)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
---
+++ task_management_app/backend/app/routes.py
@@ -0,0 +1,6 @@
+from app import app
+
+
+@app.route('/')
+def index():
+    return "Hello, Task Management App!"
